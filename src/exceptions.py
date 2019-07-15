bdaa403a11d18bc0cade1f7e1047b8dba94d4f5d
Jason <jasonjerome16@gmail.com>
2019-07-15 17:19:15 +0530

    Added custom exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,14 @@
+class DuplicateObjectNameError(Exception):
+    pass
+
+class DataMismatchError(Exception):
+    pass
+
+class JSONPackingError(Exception):
+    pass
+
+class FileNotSupportedError(Exception):
+    pass
+
+class IncorrectInputError(Exception):
+    pass
