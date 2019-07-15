ac0ac3d18a72a81e963a7300c28804fd0de88aee
Jason <jasonjerome16@gmail.com>
2019-07-15 17:19:44 +0530

    Removed unused optional parameters and allow cli input

--- src/expastack.py
+++ src/expastack.py
@@ -1,13 +1,26 @@
 import sys
-from .parameters import UserParameters
+from .utils import FileAccessUtility, CliUtility, EncodingUtility, JSONUtility
+from .exceptions import IncorrectInputError
 
 class PrimaryRunner:
+    object_path = ''
     def __init__(self):
         print("ExPaStack Initialized.")
-        if len(sys.argv) > 1:
-            print("Additional command-line parameters detected.")
-            params = UserParameters()
-            for i, arg in enumerate(sys.argv):
-                if arg in params.parameters:
-                    print(f"Command-line parameter detected - {arg[2:]}")
 
+        if len(sys.argv) > 1:
+            self.object_path = CliUtility.get_cli_parameter(sys.argv)
+        else:
+            raise IncorrectInputError('One or more parameters have not been provided to the program.\n'
+                                      'How to run: python3 <object_path> <optional_params>')
+        self.parse_and_output(self.object_path)
+
+    def parse_and_output(self, asset_path: str) -> None:
+        file_content = FileAccessUtility.get_file_content(asset_path)
+        ext_type = FileAccessUtility.get_file_extension(asset_path)
+        obj_names = FileAccessUtility.get_mesh_names(all_file_content_list=file_content, ext_type=ext_type)
+        obj_ids = EncodingUtility.encode_strings(obj_names)
+        prepped_json = JSONUtility.prepare_for_json(obj_names, obj_ids)
+        extra_check = JSONUtility.check_json(prepped_json)
+        print(f'Error Checks Passed: {extra_check}')
+        output_json = JSONUtility.pack_as_json(prepped_json)
+        print(f'Successful JSON Output: {output_json}')
