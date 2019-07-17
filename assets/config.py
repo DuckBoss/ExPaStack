3f3bd7cad35ffb1ded32bb3de9824264e573e50f
Jason <jasonjerome16@gmail.com>
2019-07-17 15:26:27 +0530

    Added comments which include the default config values

--- assets/config.py
+++ assets/config.py
@@ -1,6 +1,8 @@
 # A comma-separated list of mesh names to filter.
+# default value: filter_list = []
 filter_list = []
 
 # Filters the mesh names so that the output json contains only the filtered names(include), or everything except the filtered names(exclude)
 # This value can only be 'include' or 'exclude'.
-filter_type = ''
+# default value: filter_type = 'include'
+filter_type = 'include'
