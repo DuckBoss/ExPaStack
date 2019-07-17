5cfc696a0a4a2533b711775ac1edd2877fc7265f
Jason <jasonjerome16@gmail.com>
2019-07-17 14:24:23 +0530

    Implemented optional CL parameters

--- src/expastack.py
+++ src/expastack.py
@@ -4,16 +4,36 @@ from .exceptions import IncorrectInputError
 
 class PrimaryRunner:
     object_path = ''
+    optional_params = []
+
+    filter_list = []
+    filter_type = ''
     def __init__(self):
         print("ExPaStack Initialized.")
 
         if len(sys.argv) > 1:
             self.object_path = CliUtility.get_cli_parameter(sys.argv)
+            self.optional_params = CliUtility.get_optional_parameters(sys.argv)
+            self.process_optional_params(self.optional_params)
         else:
             raise IncorrectInputError('One or more parameters have not been provided to the program.\n'
                                       'How to run: python3 <object_path> <optional_params>')
 
         self.parse_and_output(self.object_path)
+
+    def process_optional_params(self, optional_params):
+        for i, tup in enumerate(optional_params):
+            print(tup)
+            if tup[0] == 'keywords':
+                self.filter_list = tup[1].replace(' ', '').split(',')
+                print(f'Filter List: {self.filter_list}')
+            elif tup[0] == 'filter-type':
+                self.filter_type = tup[1].replace(' ', '').strip()
+                if not (self.filter_type == 'include' or self.filter_type == 'exclude'):
+                    raise RuntimeError('The filter type must can only be "include" or "exclude"')
+                print(f'Filter Type: {self.filter_type}')
+
+
 
     def parse_and_output(self, asset_path: str) -> None:
         file_content = FileAccessUtility.get_file_content(asset_path)
@@ -24,7 +44,7 @@ class PrimaryRunner:
         # print(obj_names)
         obj_ids = EncodingUtility.encode_strings(obj_names)
         # print(obj_ids)
-        prepped_json = JSONUtility.prepare_for_json(obj_names, obj_ids)
+        prepped_json = JSONUtility.prepare_for_json(obj_names, obj_ids, self.filter_list, self.filter_type)
         extra_check = JSONUtility.check_json(prepped_json)
         print(f'Error Checks Passed: {extra_check}')
         output_json = JSONUtility.pack_as_json(prepped_json, FileAccessUtility.get_file_name(asset_path))
